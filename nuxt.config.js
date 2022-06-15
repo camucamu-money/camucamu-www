1ce4e920fddbb4f2ddb03e0c09dc1425d73f4d84
luctst <lucas.tostee@gmail.com>
2022-06-15 16:23:58 +0200

    Uptate meta tags and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,11 +10,11 @@ export default {
     cache: false,
   },
   head: {
-    title: 'camucamu-www',
+    title: 'CamuCamu - Faîtes vos comptes plus facilement.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'CamuCamu est un site web qui facilite la gestion des comptes bancaires en inscrivant manuellement vos opérations bancaires de manière sécurisée.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
